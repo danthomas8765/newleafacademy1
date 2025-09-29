db0eaecad108d6bb110986cf0dc32d96463bfd22
Dan <danthomas8765@gmail.com>
2025-09-29 17:18:06 +0100

    config fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,12 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
-
 import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://new-leaf-academy.co.uk",
+  trailingSlash: 'always', // Add this line
   integrations: [sitemap()],
   adapter: cloudflare({
     platformProxy: {
