ac980fda0bfc2b4ff91951c76fdde6f922287800
Dan <danthomas8765@gmail.com>
2025-12-05 14:28:43 +0700

    fix title

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "NEW LEAF ACADEMY";
+export const SITE_TITLE = "New Leaf Academy";
 export const SITE_DESCRIPTION = "Expert online tutoring in Maths, English, and Science. Personalized, confidence-building tuition from KS3 to A-Level. Grow with confidence.";
